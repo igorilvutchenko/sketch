343056172679570554dbe0bb0a7d1353e17c3225
Igor Ilvutchenko <corvuscorax84@gmail.com>
2018-09-24 02:29:09 +0300

    upload the project

---
+++ src/js/bootstrap.js
@@ -0,0 +1,15 @@
+
+//= ./bootstrap/alert.js
+//= ./bootstrap/button.js
+//= ./bootstrap/carousel.js
+//= ./bootstrap/collapse.js
+//= ./bootstrap/dropdown.js
+
+//= ./bootstrap/modal.js
+//= ./bootstrap/tab.js
+
+//= ./bootstrap/scrollspy.js
+//= ./bootstrap/tooltip.js
+//= ./bootstrap/popover.js
+//= ./bootstrap/util.js
+//= ./scripts.js
